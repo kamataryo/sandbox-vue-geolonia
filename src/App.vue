c70d3049b9edeaf6a31e12caef6eb0c69f31391b
kamataryo <kamataryo@users.noreply.github.com>
2021-10-28 11:45:44 +0900

    Init geolonia

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <GeoloniaMap />
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import GeoloniaMap from './components/GeoloniaMap.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    GeoloniaMap,
   }
 }
 </script>
---
+++ src/components/GeoloniaMap.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="geolonia">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'GeoloniaMap',
+  props: {},
+  mounted() {
+	const geoloniaEmbed = document.createElement('script')
+	geoloniaEmbed.setAttribute('src', 'https://cdn.geolonia.com/v1/embed?geolonia-api-key=YOUR-API-KEY')
+	document.body.append(geoloniaEmbed)
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.geolonia {
+	margin: auto;
+	width: 300px;
+	height: 300px;
+}
+</style>
